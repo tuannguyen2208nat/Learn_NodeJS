e070ea1a5941f5a5b6de10e6f946818b6b2db037
Nguyễn Anh Tuấn <tuan.nguyen2208nat@hcmut.edu.vn>
2024-08-11 00:17:24 +0700

    add nodemon

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 require('dotenv').config()
 
-console.log(">>> check env: ", process.env)
+// console.log(">>> check env: ", process.env)
 
 const app = express()
 const port = process.env.PORT || 3000;
@@ -13,7 +13,7 @@ app.set('views', path.join(__dirname, './views/'))
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World!123')
 })
 
 app.get('/abc', (req, res) => {
