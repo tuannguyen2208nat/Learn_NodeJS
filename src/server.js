af2d1b8d1617d079b370dbc3b427c7dd24eacd09
Nguyễn Anh Tuấn <tuan.nguyen2208nat@hcmut.edu.vn>
2024-08-11 00:35:03 +0700

    add public folder

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+const e = require('express');
 const express = require('express')
 const path = require('path')
 require('dotenv').config()
@@ -8,10 +9,14 @@ const app = express()
 const port = process.env.PORT || 3000;
 const hostname = process.env.HOST_NAME;
 
-
+//config template engine
 app.set('views', path.join(__dirname, './views/'))
 app.set('view engine', 'ejs')
 
+//config static folder
+app.use(express.static(path.join(__dirname, 'public')))//khai báo là tất cả file ảnh, thư mục public sẽ được truy cập từ bên ngoài
+
+//khai báo router
 app.get('/', (req, res) => {
     res.send('Hello World!123')
 })
